62539b1af5985ddbcb5a87360c04e0820dafd9dd
Christian Fraser <crfraser@alaska.edu>
2016-11-17 10:54:47 -0900

    added random map generation

--- graphics/sfml.cpp
+++ graphics/sfml.cpp
@@ -1,14 +1,15 @@
 #include "project.h"
 
 std::string playerStat(std::stringstream & s, Actor & a) {
-	s << a.getHealth();
+	s <<"HP: "<< a.getHealth();
 	return s.str();
 }
 class TileMap : public sf::Drawable, public sf::Transformable
 {
+	
 public:
 
-    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    bool load(const std::string& tileset, sf::Vector2u tileSize, std::vector<int> tiles, unsigned int width, unsigned int height)
     {
         // load the tileset texture
         if (!m_tileset.loadFromFile(tileset))
@@ -65,19 +66,38 @@ private:
     sf::VertexArray m_vertices;
     sf::Texture m_tileset;
 };
+std::vector<int> levelGen() {
+	std::vector<int> level(128);
+	level[0]=1;
+	for(int i=1;i<127;i++) {
+		level[i]=(rand()%2);
+	}
+	return level;
+}
+TileMap mapInit(std::vector<int> level) {
+	
+	TileMap map;
+	if(!map.load("tileset_cave.png",sf::Vector2u(32,32), level,16,8)) {
+		return map;
+	}
+	return map;
+}
+
+
 int main()
 {
 	srand(time(0));
 	Player player;
 	Enemy zombie1;
-    sf::RenderWindow window(sf::VideoMode(512,256), "Project Window");
-    
-    sf::RectangleShape rectangle(sf::Vector2f(32,32));//top wall
-
+    sf::RenderWindow window(sf::VideoMode(512,288), "Project Window");
+    sf::RectangleShape doorBox(sf::Vector2f(32,32));
+    doorBox.setPosition(0,0);
+	sf::RectangleShape rectangle(sf::Vector2f(512,32));//healthbox
+	rectangle.setPosition(0,256);
 	
 	//the tiles for the map, see picture for details
 	//0=water,1=tree,2=grass/bottom bridge, 3= grass/bottom water, 4=grass/top bridge, 5=grass,6=bridge,7=flowers,8=grass/top bridge
-	const int level[] {
+	std::vector<int> level ={
 		3, 5, 5, 1, 5, 1, 2, 3, 3, 3, 3, 5, 3, 3, 3, 3,
         0, 5, 3, 3, 3, 3, 6, 0, 0, 0, 0, 2, 0, 0, 0, 0,
         8, 5, 0, 0, 8, 0, 4, 8, 8, 8, 8, 6, 8, 8, 8, 8,
@@ -86,23 +106,24 @@ int main()
         8, 0, 5, 0, 5, 0, 8, 8, 0, 0, 1, 7, 5, 1, 1, 8,
         3, 0, 5, 0, 3, 0, 5, 5, 8, 0, 3, 3, 7, 5, 1, 1,
         0, 0, 5, 0, 0, 8, 5, 5, 3, 0, 0, 0, 1, 1, 1, 1,};
-		
-		
+    
 	TileMap map;
+
 	if(!map.load("tileset.png",sf::Vector2u(32,32), level,16,8)) {
 		return 1;
 	}
+	
 	sf::Font font;
 	if(!font.loadFromFile("LiberationMono-Bold.ttf")) {
 		return -1;
 	}
+	
 	sf::Text health;
 	health.setFont(font);
 	std::stringstream s;
 	std::string healthDisplay=playerStat(s,player);
 	health.setString(healthDisplay);
-	
-	health.setPosition(0,0);
+	health.setPosition(0,256);
 	health.setCharacterSize(24);
 	health.setColor(sf::Color::Red);
 	
@@ -114,7 +135,7 @@ int main()
 			return -1;// error...
 		}
 	sprite.setTexture(texture);
-	sprite.setPosition(0,0);
+	sprite.setPosition(448,64);
 	sprite.setScale(1,.5);
 	
 	sf::Sprite zombie;
@@ -123,7 +144,7 @@ int main()
 		return -1;
 	}
 	zombie.setTexture(zombie_texture);
-	zombie.setPosition(64,156);
+	zombie.setPosition(0,0);
 	
 	int xCoor=(sprite.getPosition().x / 32);
 	int yCoor = (int) (sprite.getPosition().y / 32);
@@ -169,10 +190,16 @@ int main()
 						break;
 			}
         }
-		std::cout<< "Health: "<<player.getHealth()<<std::endl;	
 		
         window.clear();
-        window.draw(map);
+        
+        sf::FloatRect boundingBox = sprite.getGlobalBounds();
+		sf::FloatRect zombieBox = zombie.getGlobalBounds();
+		sf::FloatRect door =doorBox.getGlobalBounds();
+       
+		//window.draw(map2);
+		window.draw(map);
+		window.draw(doorBox);
         window.draw(rectangle);
         window.draw(zombie);
         window.draw(sprite);
@@ -181,22 +208,24 @@ int main()
         //stuff needs to be drawn in the right order.
         //VERY IMPORTANT: collision detection is done after drawing
         //	if it's done before the bounds are not defined
-        sf::FloatRect boundingBox = sprite.getGlobalBounds();
-		sf::FloatRect zombieBox = zombie.getGlobalBounds();
-		s.str("");
-		if(boundingBox.intersects(zombieBox))
-		{
-			sprite.move(0,-32);
-			player.damage(1);
+        if(boundingBox.intersects(door)) {
+			level=levelGen();
+			map=mapInit(level);
+			//mapLoad(map,level);
+			std::cout << "changed map" << std::endl;
+			sprite.setPosition(448,64);
+		}
 			
-			//std::cout << "collision detected"<< std::endl;
+		if(boundingBox.intersects(zombieBox)) {
+			sprite.move(32,0);
+			player.damage(1);
 		}	
 
 		if(player.getHealth()<=0) {
 			std::cout << "You died" <<std::endl;
 			return 0;
 		}
-		
+		s.str(""); //clear the health string
     }
 
     return 0;
