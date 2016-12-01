3cfef0987df092060282c25b738f3ed3a0dd04c9
Christian Fraser <crfraser@alaska.edu>
2016-12-01 10:39:59 -0900

    fixed sprite

--- graphics/project.cpp
+++ graphics/project.cpp
@@ -18,24 +18,34 @@ int Actor::getHealth() {
 }
 
 void Enemy::updatePosition(sf::Sprite &s,std::vector<int> level) {
+	sf::IntRect walkingDown(0, 0, 32, 32);
+    sf::IntRect walkingLeft(0, 32, 32, 32);
+    sf::IntRect walkingRight(0, 64, 32, 32);
+    sf::IntRect walkingUp(0, 96, 32, 32);
 	int xCoor=(int) (s.getPosition().x / 32);
 	int yCoor = (int) (s.getPosition().y / 32);
 	int index = xCoor+yCoor*16;
 	int direction= rand()%6;					
-	if ((direction ==2) && index >= 16 && level[index-16] != 0) //check window bounds and water 
-		s.move(0,-32);	
-						
-	if (direction==4 && index % 16 != 0 && level[index-1] != 0)
+	if ((direction ==2) && index >= 16 && level[index-16] != 0) { //check window bounds and water 
+		s.move(0,-32);
+		s.setTextureRect(walkingUp);
+	}
+	if (direction==4 && index % 16 != 0 && level[index-1] != 0){
 		s.move(-32,0);								
-					
-	if (direction ==1 && index <=112 && level[index+16] != 0)
+		s.setTextureRect(walkingLeft);			
+	}
+	if (direction ==1 && index <=112 && level[index+16] != 0){
 		s.move(0,32);						
-						
-	if (direction ==3 && index % 16 != 15 && level[index+1] != 0)
+		s.setTextureRect(walkingDown);				
+	}
+	if (direction ==3 && index % 16 != 15 && level[index+1] != 0){
 		s.move(32,0);
-		
-	if (direction==5)
+		s.setTextureRect(walkingRight);
+	}
+	if (direction==5){
 		s.move(0,0);
+		s.setTextureRect(walkingUp);
+	}
 }
 
 //std::string playerStat(std::stringstream & s, Actor a) {
