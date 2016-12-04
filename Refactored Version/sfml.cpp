124bc21c7b795f60be12ac98f053f3179cad4430
thatcherlane <thatcherlane@gmail.com>
2016-12-04 14:55:57 -0900

    deleting a few extra lines

--- Refactored Version/sfml.cpp
+++ Refactored Version/sfml.cpp
@@ -264,9 +264,6 @@ int main()
         window.draw(health);
 		window.display();
         
-        Level::saveGame(map);
-        Level::loadGame(map); 
-        
 		//stuff needs to be drawn in the right order.
 		//VERY IMPORTANT: collision detection is done after drawing
 		//	if it's done before the bounds are not defined
