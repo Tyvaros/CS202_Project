7257613acab3473e1f55a51f480014c9091a2584
thatcherlane <thatcherlane@gmail.com>
2016-12-05 18:19:57 -0900

    fixing warnings

--- Refactored Version/sfml.cpp
+++ Refactored Version/sfml.cpp
@@ -54,7 +54,7 @@ int main()
 	health.setString(healthDisplay);
 	health.setPosition(0, 256);
 	health.setCharacterSize(24);
-	health.setColor(sf::Color::Red);
+	health.setFillColor(sf::Color::Red);
 
 	sf::Sprite sprite;
 	sf::Texture texture;
